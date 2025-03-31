cb145a52b6f5ed6a7e47883bf4b03b305fd5e773
Theo <sagniez.theo@laposte.net>
2025-03-31 14:16:25 +0200

    Created shuffle function + distributing cards

---
+++ scripts.js
@@ -0,0 +1,54 @@
+const families = [
+    "Grimes",
+    "Parker",
+    "Wayne",
+    "Rogers",
+    "Banner",
+    "Kent",
+    "Queen"
+]
+
+const members = [
+    "Grandfather",
+    "Grandmother",
+    "Father",
+    "Mother",
+    "Son",
+    "Daughter"
+]
+
+const allFamilies = []
+const nbPlayers = 4
+const playersCards = {}
+
+const shuffle = array => {
+    const copy = array.slice()
+    const result = []
+
+    while (copy.length > 1) {
+        const index = Math.floor(Math.random() * copy.length)
+
+        result.push(copy[index])
+
+        copy.splice(index, 1)
+    }
+
+    result.push(copy[0])
+
+    return result
+}
+
+families.forEach(family => {
+    members.forEach(member => {
+        allFamilies.push(member + ' ' + family)
+    })
+})
+
+const cardsShuffled =  shuffle(allFamilies)
+
+for (let i = 1; i <= nbPlayers; i++) {
+    playersCards[`player_${i}`] = cardsShuffled.splice(0, 7)
+}
+
+console.log(playersCards)
+console.log(cardsShuffled)
